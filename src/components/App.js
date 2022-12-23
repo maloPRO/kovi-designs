e6317ec6d3b2652466519f47f558dddccac99b8a
Gilbert Malova <malovagilbert@gmail.com>
2022-12-23 21:00:08 +0300

    Added app boilertplate files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/app.css';
+import Nav from "./nav";
+
+const App = () => {
+  
+
+  return (
+    <div className="app">
+      <Nav />
+      
+    </div>
+  )
+}
+
+export default App;
