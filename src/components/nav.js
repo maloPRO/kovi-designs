afc8472a4186a5bfab8854e50c898b9eb47c5453
Gilbert Malova <malovagilbert@gmail.com>
2022-12-23 22:50:09 +0300

    Nav styling edits

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,10 +5,16 @@ const Nav = () => {
 
   return (
     <div className="nav">
+      <div className='siteDetails'>
       <div className='siteName'>Kovi <span>Designs</span></div>
       <div className='siteSlogan'>
         <p>Modern furniture</p>
         <p>for home & office</p>
+      </div>
+      </div>
+      <div className='comms'>
+        <div>About</div>
+        <div>Contact</div>
       </div>
     </div>
   )
