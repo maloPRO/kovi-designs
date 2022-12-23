02e9373eb1ddea4c3e8f4eb5b81afc3660054e29
Gilbert Malova <malovagilbert@gmail.com>
2022-12-23 20:58:49 +0300

    Added navbar component

---
+++ src/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/nav.css';
+
+const Nav = () => {
+
+  return (
+    <div className="nav">
+      <div className='siteName'>Kovi <span>Designs</span></div>
+      <div className='siteSlogan'>
+        <p>Modern furniture</p>
+        <p>for home & office</p>
+      </div>
+    </div>
+  )
+
+}
+
+export default Nav;
