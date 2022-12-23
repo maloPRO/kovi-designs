0f8ccd2394a8c676c1ccacaac22363e124506be1
Gilbert Malova <malovagilbert@gmail.com>
2022-12-23 22:51:28 +0300

    Added hero section

---
+++ src/components/homeDisplay.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect} from "react";
+import '../styles/homeDisplay.css';
+import bg from '../images/bg/bg.jpg';
+import bg1 from '../images/bg/bg1.jpg';
+import bg2 from '../images/bg/bg2.jpg';
+
+const images = [bg, bg1, bg2];
+const HomeDisplay = () => {
+  
+  const [background, setBackground] = useState(images[Math.floor(Math.random() * images.length)]);
+  
+
+  return (
+    <div className="homeDisplay" style={{backgroundImage: `url(${background})`}}>Shop high-end furniture now</div>
+  )
+}
+
+export default HomeDisplay;
