7c3894e50270f5489c8ee8cdf74124de5b0bcafa
Gilbert Malova <malovagilbert@gmail.com>
2022-12-23 22:52:51 +0300

    Linked component edits to App

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import React from "react";
 import '../styles/app.css';
+import uniqid from 'uniqid';
 import Nav from "./nav";
+import Categories from "./categories";
+import HomeDisplay from "./homeDisplay";
 
 const App = () => {
-  
+  const categories = [
+    {
+      name: 'bedroom',
+      id: uniqid()
+    },
+    {
+      name: 'dining',
+      id: uniqid()
+    },
+    {
+      name: 'living',
+      id: uniqid()
+    },
+    {
+      name: 'office',
+      id: uniqid()
+    },
+    {
+      name: 'kitchen',
+      id: uniqid()
+    }
+  ]
 
   return (
     <div className="app">
-      <Nav />
-      
+      <Nav />  
+      <Categories categories = {categories} />
+      <HomeDisplay />
     </div>
   )
 }
