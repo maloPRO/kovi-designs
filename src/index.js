896efddaf5131c52a8569cbce88739f0af4cd8ae
Gilbert Malova <malovagilbert@gmail.com>
2022-12-24 14:52:25 +0300

    Added router component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./components/App";
 import './styles/index.css';
+import Beds from "./components/beds";
+import Sofas from "./components/sofas";
+import Dining from "./components/dining";
+
+const router  = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>
+  },
+  {
+    path: '/beds',
+    element: <Beds />
+  },
+  {
+    path: '/sofas',
+    element: <Sofas />
+  },
+  {
+    path: '/dining',
+    element: <Dining />
+  }
+])
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router = {router} />
   </React.StrictMode>,
   document.getElementById('root')
 );
