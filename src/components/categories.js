de8ee884e7ca1ebb6074889b7162ab2820c27ae2
Gilbert Malova <malovagilbert@gmail.com>
2022-12-24 14:49:36 +0300

    Included categories state

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,8 +1,32 @@
 import React, { useState } from "react";
+import uniqid from 'uniqid';
 import '../styles/categories.css';
 
 const Categories = (props) => {
-  const [categories , setCategories] = useState(props.categories);
+  const [categories] = useState(
+     [
+      {
+        name: 'bedroom',
+        id: uniqid()
+      },
+      {
+        name: 'dining',
+        id: uniqid()
+      },
+      {
+        name: 'living',
+        id: uniqid()
+      },
+      {
+        name: 'office',
+        id: uniqid()
+      },
+      {
+        name: 'kitchen',
+        id: uniqid()
+      }
+    ]
+  );
 
   const categoryList = categories.map((category) => 
     <li key={category.id}>{category.name}</li>
