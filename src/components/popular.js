6b3e766160afaa40b1b433e902884d6f6f3fd56d
Gilbert Malova <malovagilbert@gmail.com>
2022-12-27 02:01:25 +0300

    Fixed links to popular categories

--- src/components/popular.js
+++ src/components/popular.js
@@ -12,9 +12,9 @@ const Popular = () => {
     <div className="popular">
       <h3>Popular categories</h3>
       <div className="popContainer">
-        <Link className="links" to = {`/sofas`} style={{backgroundImage: `url(${sofaBg})`}}>Sofas</Link>
-        <Link className="links" to = {`/beds`} style={{backgroundImage: `url(${bedBg})`}}>Beds</Link>
-        <Link className="links" to = {`/dining`} style={{backgroundImage: `url(${dinnerBg})`}}>Dining</Link>
+        <Link className="links" to = {`${process.env.PUBLIC_URL}/sofas`} style={{backgroundImage: `url(${sofaBg})`}}>Sofas</Link>
+        <Link className="links" to = {`${process.env.PUBLIC_URL}/beds`} style={{backgroundImage: `url(${bedBg})`}}>Beds</Link>
+        <Link className="links" to = {`${process.env.PUBLIC_URL}/dining`} style={{backgroundImage: `url(${dinnerBg})`}}>Dining</Link>
       </div>
     </div>
   )
